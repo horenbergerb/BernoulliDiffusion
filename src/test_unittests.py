58eb7ffd65bc57a1612395ad468ef584989f7d1e
horenbergerb <horenbergerbeau@gmail.com>
2022-09-29 19:53:10 -0400

    "got all unit tests to pass

--- src/test_unittests.py
+++ src/test_unittests.py
@@ -1,6 +1,7 @@
 import torch
 
 import unittest
+import statistics
 
 from src.data import sample_heartbeat, generate_batch
 from src.model import ReverseModel, BernoulliDiffusion
@@ -66,7 +67,6 @@ class TestBinomialDiffusion(unittest.TestCase):
             self.cfg.T = T
             reverse_model = ReverseModel(self.cfg.sequence_length, self.cfg.T).to(device)
             diffusion_model = BernoulliDiffusion(reverse_model, self.cfg.sequence_length, self.cfg.num_sample_steps, self.cfg.T).to(device)
-            print('T: {} beta_tilde_T: {}'.format(T, diffusion_model.beta_tilde_t[T][0].item()))
             self.assertAlmostEqual(1.0, diffusion_model.beta_tilde_t[T][0].item(), 5)
             self.assertAlmostEqual(diffusion_model.beta_t(1), diffusion_model.beta_tilde_t[1][0].item(), 5)
 
@@ -75,47 +75,55 @@ class TestBinomialDiffusion(unittest.TestCase):
         p(1->1): 1/5*(1-0.5*beta_tilde_t)
         p(0->1): 4/5*(0.5*beta_tilde_t)
         p(1) for digits of x_t = (20/100*(1-0.5*beta_tilde_t)) + (80/100*(0.5*beta_tilde_t))'''
-        x_0 = generate_batch(num_samples=self.cfg.batch_size,
-                               period=self.cfg.period,
-                               sequence_length=self.cfg.sequence_length).to(device)
 
-        for t in [10, 100, 500, 1000, 2000]:
-            result = self.diffusion_model.q_sample(x_0, t)
+        target_t = 1000
+
+        beta_tilde_t = self.diffusion_model.beta_tilde_t[target_t][0].item()
+        expectation = (20.0/100.0*(1.0-0.5*beta_tilde_t)) + (80.0/100.0*(0.5*beta_tilde_t))
+        results = []
+
+        for cur_sample in range(1000):
+            x_0 = generate_batch(num_samples=self.cfg.batch_size,
+                    period=self.cfg.period,
+                    sequence_length=self.cfg.sequence_length).to(device)
+            result = self.diffusion_model.q_sample(x_0, target_t)
             result = torch.mean(result).item()
+            results.append(result)
 
-            beta_tilde_t = self.diffusion_model.beta_tilde_t[t][0].item()
-            expectation = (20.0/100.0*(1.0-0.5*beta_tilde_t)) + (80.0/100.0*(0.5*beta_tilde_t))
-            
-            err_msg = 'beta_tilde_t: {}, {} and {} are not almost equal'.format(beta_tilde_t, result, expectation)
-
-            self.assertAlmostEqual(result, expectation, 5, err_msg)
+        err_msg = 'beta_tilde_t: {}, {} and {} are not almost equal'.format(beta_tilde_t, result, expectation)
+        self.assertAlmostEqual(statistics.fmean(results), expectation, 5, err_msg)
 
             
     def test_sampling_methods_agree(self):
         '''The proportion of digits which are 1 should be approximately equivalent whether we iterate sampling
         from x_0->x_1->...->x_t or sample directly from x_t'''
-        x_0 = generate_batch(num_samples=self.cfg.batch_size,
-                               period=self.cfg.period,
-                               sequence_length=self.cfg.sequence_length).to(device)
 
         target_t = 500
-        result1 = x_0
-        for t in range(0, target_t):
-            # print('beta_tilde_{}: {}'.format(t, diffusion_model.beta_tilde_t[t][0].item()))
-            # print('beta_t_{}: {}'.format(t, diffusion_model.beta_t(t)))
-            result1 = self.diffusion_model.q_step(result1, t)
 
-        result2 = self.diffusion_model.q_sample(x_0, target_t)
+        results1 = []
+        results2 = []
 
-        result1 = torch.mean(result1).item()
-        result2 = torch.mean(result2).item()
+        for cur_sample in range(1000):
+            x_0 = generate_batch(num_samples=self.cfg.batch_size,
+                                period=self.cfg.period,
+                                sequence_length=self.cfg.sequence_length).to(device)
+
+            result1 = x_0
+            for t in range(0, target_t):
+                # print('beta_tilde_{}: {}'.format(t, diffusion_model.beta_tilde_t[t][0].item()))
+                # print('beta_t_{}: {}'.format(t, diffusion_model.beta_t(t)))
+                result1 = self.diffusion_model.q_step(result1, t)
+
+            result2 = self.diffusion_model.q_sample(x_0, target_t)
+
+            result1 = torch.mean(result1).item()
+            result2 = torch.mean(result2).item()
+            results1.append(result1)
+            results2.append(result2)
         
-        err_msg = '{} and {} are not almost equal'.format(result1, result2)
-
-        self.assertAlmostEqual(result1, result2, 5, err_msg)
-                
-        # print(diffusion_model.beta_tilde_t[target_t][0])
-        
+        err_msg = '{} and {} are not almost equal'.format(statistics.fmean(results1), statistics.fmean(results2))
+        self.assertAlmostEqual(statistics.fmean(results1), statistics.fmean(results2), 5, err_msg)
+                        
 
 if __name__ == '__main__':
     unittest.main()
