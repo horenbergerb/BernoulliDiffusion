d4c22969e46865dee4541c2d364388185ecb6df3
horenbergerb <horenbergerbeau@gmail.com>
2022-10-06 17:51:07 -0400

    Redesigned data pipeline

--- BernoulliDiffusion/config.py
+++ BernoulliDiffusion/config.py
@@ -1,27 +1,39 @@
 from dataclasses import dataclass
 import yaml
 
+
 @dataclass
-class Config:
-    sequence_length: int
-    period: int
+class ModelConfig:
     T: int
-    batch_size: int
-    num_batches: int
     num_sample_steps: int
+
+
+@dataclass
+class TrainingConfig:
+    period: int
     epochs: int
     lr: float
     training_info_freq: int
+    num_examples: int
     save_every_n_epochs: int
     output_dir: str
 
 
-def load_config(cfg_dir: str) -> Config:
+@dataclass
+class DataConfig:
+    data_dir: str
+    batch_size: int
+
+
+def load_config(cfg_dir: str):
     cfg_dict = None
     with open(cfg_dir, "r") as stream:
         try:
             cfg_dict = yaml.safe_load(stream)
+            model_cfg = ModelConfig(**cfg_dict['model'])
+            training_cfg = TrainingConfig(**cfg_dict['training'])
+            data_cfg = DataConfig(**cfg_dict['data'])
         except yaml.YAMLError as exc:
             print(exc)
             exit()
-    return Config(**cfg_dict)
+    return model_cfg, training_cfg, data_cfg
