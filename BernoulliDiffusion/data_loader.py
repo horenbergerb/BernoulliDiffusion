Correct posterior calculation, change optimizer, bug fixes (#29)
* bug fixes, new dataset

* fixed posterior and corrected kl div, changed optimizer to adamw

BernoulliDiffusion/data_loader.py
@@ -31,10 +31,12 @@ class DataLoader:
             self.cur_ind = 0
             return None
 
-    def generate_random_data(self):
+    def generate_random_data(self, batch_size=None):
         '''Generates a random tensor with the same shape as training data.
         Used for initialization'''
-        m = Binomial(1, torch.zeros((self.cfg.batch_size, self.sequence_length)).fill_(0.5))
+        if batch_size is None:
+            batch_size = self.cfg.batch_size
+        m = Binomial(1, torch.zeros((batch_size, self.sequence_length)).fill_(0.5))
         return m.sample().to(device)
 
     def get_sequence_length(self):

BernoulliDiffusion/diffusion_model.py
@@ -7,6 +7,7 @@ from BernoulliDiffusion.reverse_model import ReverseModel
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
+print('Using device {}'.format(device))
 
 class BernoulliDiffusionModel(nn.Module):
     def __init__(self, sequence_length, num_sample_steps, T):
@@ -45,6 +46,8 @@ class BernoulliDiffusionModel(nn.Module):
         if x is None:
             init_prob = torch.empty((batch_size, self.sequence_length)).fill_(0.5).to(device)
             x = torch.bernoulli(init_prob)
+        else:
+            assert batch_size == x.size(dim=0)
 
         for cur_t in range(self.T, 0, -1):
             x = torch.bernoulli(self.p_conditional_prob(x, cur_t))
@@ -85,7 +88,9 @@ class BernoulliDiffusionModel(nn.Module):
             x_t = self.q_sample(x_0, t)
             beta_t = self.beta_t(t)
             posterior = x_0*(1-self.beta_tilde_t[t-1]) + 0.5*self.beta_tilde_t[t-1]
-            posterior *= x_t * (1-0.5*beta_t) + (1 - x_t) * (1.5*beta_t)
+            posterior *= x_t * (1-0.5*beta_t) + (1 - x_t) * (0.5*beta_t)
+            normalizing_constant = x_t * self.q_conditional_prob_wrt_x_0(x_0, t) + (1-x_t) * (1-self.q_conditional_prob_wrt_x_0(x_0, t))
+            posterior = posterior / normalizing_constant
             kl_divergence = kl_div(posterior,
                                    self.p_conditional_prob(x_t, t))
 

BernoulliDiffusion/tests/test_model.py
@@ -40,25 +40,25 @@ class TestBinomialDiffusion(unittest.TestCase):
         p(0->1): (1 - 1/period)*(0.5*beta_tilde_t)
         p(1) for digits of x_t = ((1/period)*(1-0.5*beta_tilde_t)) + ((1 - 1/period)(0.5*beta_tilde_t))'''
 
-        target_t = 1000
-        period = 100
-        prob_1 = 1/period
+        for target_t in [250, 500, 1000, 150]:
+            period = 100
+            prob_1 = 1/period
 
-        beta_tilde_t = self.diffusion_model.beta_tilde_t[target_t][0].item()
-        expectation = (prob_1*(1.0-0.5*beta_tilde_t)) + ((1-prob_1)*(0.5*beta_tilde_t))
-        results = []
+            beta_tilde_t = self.diffusion_model.beta_tilde_t[target_t][0].item()
+            expectation = (prob_1*(1.0-0.5*beta_tilde_t)) + ((1-prob_1)*(0.5*beta_tilde_t))
+            results = []
 
-        for cur_sample in range(10000):
-            x_0 = self.data_loader.next_minibatch()
-            while x_0 is not None:
-                result = self.diffusion_model.q_sample(x_0, target_t)
-                result = torch.mean(result).item()
-                results.append(result)
+            for cur_sample in range(10000):
                 x_0 = self.data_loader.next_minibatch()
+                while x_0 is not None:
+                    result = self.diffusion_model.q_sample(x_0, target_t)
+                    result = torch.mean(result).item()
+                    results.append(result)
+                    x_0 = self.data_loader.next_minibatch()
 
 
-        err_msg = 'beta_tilde_t: {}, {} and {} are not almost equal'.format(beta_tilde_t, result, expectation)
-        self.assertAlmostEqual(statistics.fmean(results), expectation, 4, err_msg)
+            err_msg = 'beta_tilde_t: {}, {} and {} are not almost equal'.format(beta_tilde_t, result, expectation)
+            self.assertAlmostEqual(statistics.fmean(results), expectation, 4, err_msg)
             
     def test_sampling_methods_agree(self):
         '''The proportion of digits which are 1 should be approximately equivalent whether we iterate sampling

BernoulliDiffusion/utils/math_utils.py
@@ -5,14 +5,16 @@ eps = 1e-30
 
 def kl_div(q, p):
     '''KL Divergence of two multivariate Bernoulli distributions'''
-    return torch.sum((q * torch.log2((q/p) + eps)) + ((1-q) * torch.log2(((1.0-q)/(1.0-p)) + eps)), dim=1)
+    q = torch.clip(q, min=1e-10, max=1-(1e-7))
+    p = torch.clip(p, min=1e-10, max=1-(1e-7))
+    return torch.sum((q * torch.log2((q/p) + eps)) + ((1.0-q) * torch.log2(((1.0-q)/(1.0-p)) + eps)), dim=1)
 
 
 def entropy_of_q_conditional(sequence_length, beta_tilde_t):
     total_entropy = 0.0
     for k in range(0, sequence_length+1):
         n_choose_k = comb(sequence_length, k)
-        prob = pow((1-(0.5*beta_tilde_t)), k) * pow(0.5*beta_tilde_t, sequence_length-k)
+        prob = pow((1.0-(0.5*beta_tilde_t)), k) * pow(0.5*beta_tilde_t, sequence_length-k)
         cur_entropy = n_choose_k * prob * log2(prob + eps)
         total_entropy += cur_entropy
     return -1.0 * total_entropy
@@ -24,7 +26,7 @@ def entropy_of_prior(sequence_length):
     total_entropy = 0.0
     for k in range(0, sequence_length+1):
         n_choose_k = comb(sequence_length, k)
-        prob = pow((1-0.5), k) * pow(0.5, sequence_length-k)
+        prob = pow((1.0-0.5), k) * pow(0.5, sequence_length-k)
         cur_entropy = n_choose_k * prob * log2(prob + eps)
         total_entropy += cur_entropy
     return -1.0 * total_entropy